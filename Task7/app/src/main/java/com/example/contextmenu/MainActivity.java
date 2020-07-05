9918c9cba712f229928d11efd031f26ad1a77af1
sri-harsh <44131849+sri-harsh@users.noreply.github.com>
2020-07-05 18:20:38 +0530

    final

---
+++ Task7/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.contextmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.ContextMenu);
+        registerForContextMenu(b1);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menufile, menu);
+    }
+
+    public void menu(View view) {
+        Toast.makeText(this, "Long press to display options", Toast.LENGTH_SHORT).show();
+    }
+}
